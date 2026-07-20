0752d619b06ac34bf16769e1eba9b12f45eef6a9
Letícia Geovana Lopes dos Santos <leticiageovana335@gmail.com>
2026-07-20 08:53:13 -0300

    refactor: Mudança de senha do administrador para comportar o número mínimo de seis caracteres

--- backend/Helpers/DataSeeder.cs
+++ backend/Helpers/DataSeeder.cs
@@ -1,5 +1,6 @@
 using backend_petshop.Data;
 using backend_petshop.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend_petshop.Helpers
 {
@@ -7,17 +8,16 @@ namespace backend_petshop.Helpers
     {
         public static async Task SeedAdminUser(AppDbContext context)
         {
-            if (!context.Usuarios.Any(u => u.Login == "admin"))
-            {
-                var admin = new Usuario
-                {
-                    Login = "admin",
-                    SenhaHash = BCrypt.Net.BCrypt.HashPassword("admin")
-                };
+            var admin = await context.Usuarios.FirstOrDefaultAsync(u => u.Login == "admin");
 
+            if (admin == null)
+            {
+                admin = new Usuario { Login = "admin" };
                 context.Usuarios.Add(admin);
-                await context.SaveChangesAsync();
             }
+
+            admin.SenhaHash = BCrypt.Net.BCrypt.HashPassword("administracao");
+            await context.SaveChangesAsync();
         }
     }
 }
